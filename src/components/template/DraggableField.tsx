16ebeb0b5221a7d098e1906870216938bce65e36
Krish Patel <krishtopher1239@gmail.com>
2026-01-19 16:58:58 +0000

    removed unused state from DraggableField

--- src/components/template/DraggableField.tsx
+++ src/components/template/DraggableField.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDraggable } from '@dnd-kit/core';
 import { CSS } from '@dnd-kit/utilities';
 import { FieldType } from '../../types';
