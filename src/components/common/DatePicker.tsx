b4fc74979be4b4e7efe0f903a8b881137f082225
Krish Patel <krishtopher1239@gmail.com>
2026-03-15 16:08:43 +0000

    debiggred unused vars

--- src/components/common/DatePicker.tsx
+++ src/components/common/DatePicker.tsx
@@ -13,7 +13,6 @@ const MONTHS = [
   'January', 'February', 'March', 'April', 'May', 'June',
   'July', 'August', 'September', 'October', 'November', 'December',
 ];
-const MONTHS_SHORT = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 type View = 'calendar' | 'year';
 
